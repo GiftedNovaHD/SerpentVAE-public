010513dd69907a7155ab9385a2ce1a66a1131a55
PuR3Luck <aidan.ong822@gmail.com>
2025-02-05 01:15:55 +0800

    initial commit for new qnet
    Co-authored-by: GiftedNova <GiftedNovaHD@users.noreply.github.com>

--- serpentvae/modules/qnet.py
+++ serpentvae/modules/qnet.py
@@ -1,18 +1,25 @@
 import torch
 import torch.nn as nn
+from mlp import MLP
 
 class QNet(nn.Module):
   def __init__(self, 
-               decoder_hidden_dim, 
+               q_net_inner_dim,
                latent_dim
                ):
     super(QNet, self).__init__() 
+    
+    # NOTE: Since context latent dim is same as current latent dim, we opt to multiply by 2
+    self.mlp = MLP(latent_dim * 2, inner_dim = q_net_inner_dim)
+
+
     # TODO: Relook at QNet implementation
 
     # Map the decoder hidden state to 2 * latent_dim for the Gaussian mean and log variance
-    self.fc = nn.Linear(decoder_hidden_dim, latent_dim * 2)
+    self.fc = nn.Linear(latent_dim * 2, latent_dim * 2)
     
-  def forward(self, h):
-    q_params = self.fc(h)
-    mu_q, logvar_q = torch.chunk(q_params, 2, dim=-1)
-    return mu_q, logvar_q
+  def forward(self, decoder_output, conditioning_latent):
+    raise NotImplementedError
+    # q_params = self.fc(h)
+    # mu_q, logvar_q = torch.chunk(q_params, 2, dim=-1)
+    # return mu_q, logvar_q
