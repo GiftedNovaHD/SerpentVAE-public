8bcaee9e3500f7a28b843ae6a10d87d9d6bd660c
GiftedNova <cai_junxiang@outlook.com>
2025-02-08 01:35:30 +0800

    Rewrite qnet.py
    Co-authored-by: Aidan Ong <PuR3Luck@users.noreply.github.com>

--- serpentvae/modules/qnet.py
+++ serpentvae/modules/qnet.py
@@ -1,13 +1,14 @@
 import torch
 import torch.nn as nn
 from torch import Tensor
-from typing import List
+from typing import Optional
+
 from modules.encoder import Encoder
+from modules.tied_linear import TiedLinear
 
 class QNet(nn.Module):
   def __init__(self,
-               latent_dim: int, 
-               hidden_dim: int,
+               latent_dim: int,
                num_layers: int,
                conv_length: int, 
                mamba_expand: int, 
@@ -16,7 +17,8 @@ class QNet(nn.Module):
                residual_in_fp32: bool = False, 
                device=None, 
                dtype=None, 
-               vocab_size: int = None,
+               vocab_size: Optional[int] = None,
+               hidden_dim: Optional[int] = None
               ):
     """
     Args: 
@@ -29,18 +31,30 @@ class QNet(nn.Module):
     super(QNet, self).__init__() 
     state_dim = state_dim if state_dim is not None else latent_dim
 
-    # Optional: if vocab_size is provided, we introduce an embedding layer
-    if vocab_size is not None:
-      self.embedding = nn.Embedding(vocab_size, hidden_dim)
-      # Project decoder logits to latent_dim
-      self.decoder_proj = nn.Linear(vocab_size, latent_dim)
-    else:
-      self.embedding = None
+    # Make sure that only vocab_size or hidden_dim is enabled at a single time
+    assert vocab_size is not None ^ hidden_dim is not None, "Either vocab_size or hidden_dim must be provided, but not both at the same time"
     
+    if vocab_size is not None:
+      self.discrete = True
+    elif hidden_dim is not None:
+      self.discrete = False
+    else:
+      raise ValueError("Either vocab_size or hidden_dim must be provided, but not both at the same time")
+    
+    # Optional: if vocab_size is provided, we introduce an embedding layer
+    if self.discrete is True:
+      self.embedding = nn.Embedding(vocab_size, latent_dim)
+      # Tie the embedding layer with the decoder
+      self.decoder_proj = TiedLinear(self.embedding)
+    
+    # Else: if hidden_dim is provided (instead of vocab_dim), we project the hidden_dim to the latent_dim 
+    elif self.discrete is False: # We are not using discrete tokens - Set hidden_dim if using semi-sparse vectors eg from Poisson-VAE or dense vectors
+      self.input_proj = nn.Linear(hidden_dim, latent_dim)
+
     # NOTE: Since context latent dim is same as current latent dim, we concatenate them (hence multiply by 2) and pass it through the MLP
     self.seq_mixer = Encoder(
       num_layers=num_layers, 
-      hidden_dim=hidden_dim,
+      hidden_dim=latent_dim,
       state_dim=state_dim,
       conv_length=conv_length,
       mamba_expand=mamba_expand,
@@ -55,9 +69,6 @@ class QNet(nn.Module):
     self.mu_head = nn.Linear(latent_dim, latent_dim) 
     self.logvar_head = nn.Linear(latent_dim, latent_dim) 
     
-    # TODO: Tighten the assumption. 
-    # Right now we assume hidden_dim == latent_dim. 
-    # If the output of the seq_mixer is in hidden_dim and we want latent_dim, we might include an affine projection. 
   def forward(self, 
               decoder_output: Tensor, 
               input_ids: Tensor,
@@ -93,82 +104,75 @@ class QNet(nn.Module):
     """
     B, L, _ = input_ids.shape
 
-    # Step 1: Generate context representations from input_ids 
-    if self.embedding is not None:
+    """
+    Step 1: Generate context representations from input_ids
+    """
+    if self.discrete is True: # In case where there is input is discrete
       input_ids_squeezed = input_ids.squeeze(dim=-1) # (batch_size, seq_len, 1) -> (batch_size, seq_len)
-      context_embeddings = self.embedding(input_ids_squeezed) # (batch_size, seq_len, hidden_dim) 
-    else: 
-      # If no embedding layer, we assume that input_ids already have shape (batch_size, seq_len, hidden_dim)
-      context_embeddings = input_ids
-
-    context_states = self.seq_mixer(context_embeddings) # (batch_size, seq_len, hidden_dim) -> (batch_size, seq_len, hidden_dim)
-
-    if self.decoder_proj is not None: 
-      decoder_states = self.decoder_proj(decoder_output) # (batch_size, seq_len, vocab_size) -> (batch_size, seq_len, latent_dim)
-    else: 
-      decoder_states = decoder_output
-
-    # Step 2: Extract subsequence representations for both decoder_output and context_states 
-    aggregated_decoder = [] 
-    aggregated_context = [] 
-    seg_counts = [] # Number of segments per sample
-
-    for b in range(B): 
-      # Get segmentation mask for sample b 
-      seg_mask = segmentation_indices[b].squeeze(-1).bool() # (batch_size, seq_len, 1) -> (seq_len,)
-      seg_start_indices = torch.nonzero(seg_mask, as_tuple=False).squeeze(-1) # (seq_len,) -> (num_segments,)
-      sample_decoder = decoder_states[b] # (seq_len, latent_dim)
-      sample_context = context_states[b] # (seq_len, hidden_dim)
-      sample_decoder_representations = []
-      sample_context_representations = []
-      num_segments = seg_start_indices.numel()
-      
-      if num_segments == 0: 
-        sample_decoder_representations.append(torch.zeros(decoder_output.size(-1), device=decoder_output.device))
-        sample_context_representations.append(torch.zeros(context_states.size(-1), device=context_states.device))
-      else: 
-        for i, start in enumerate(seg_start_indices): 
-          if i + 1 < num_segments: 
-            end = seg_start_indices[i + 1] - 1 
-          else: 
-            end = L - 1 
-          
-          decoder_subsequence = sample_decoder[start: end + 1, :] # (seq_len, vocab_size) -> (subseq_len, vocab_size)
-          decoder_representation = decoder_subsequence[-1, :] # (latent_dim,)
-          sample_decoder_representations.append(decoder_representation)
-          
-          context_subsequence = sample_context[start: end + 1, :] # (seq_len, latent_dim) -> (subseq_len, latent_dim)
-          context_representation = context_subsequence[-1, :] # (latent_dim,)
-          sample_context_representations.append(context_representation) # (latent_dim,)
-        sample_decoder_representations = torch.stack(sample_decoder_representations, dim=0) # (num_segments, latent_dim)
-        sample_context_representations = torch.stack(sample_context_representations, dim=0) # (num_segments, latent_dim)
-        aggregated_decoder.append(sample_decoder_representations)
-        aggregated_context.append(sample_context_representations)
-        seg_counts.append(sample_decoder_representations.size(0))
+      correct_embeddings = self.embedding(input_ids_squeezed) # (batch_size, seq_len, latent_dim)
+      # NOTE: Since embedding does not work on the logits we used a tied linear layer to project logits to the same embedding space
+      decoder_embeddings = self.decoder_proj(decoder_output) # (batch_size, seq_len, vocab_size) -> (batch_size, seq_len, latent_dim)
     
-    # Pad the representations so that every sample has max_segments
-    max_segments = max(seg_counts)
-    padded_decoder = [] 
-    padded_context = []
+    else:
+      # NOTE: We share the same projection layer for both the input_ids and the decoder_output to ensure that it uses a shared embedding space
+      correct_embeddings = self.input_proj(input_ids) # (batch_size, seq_len, hidden_dim) -> (batch_size, seq_len, latent_dim)
+      decoder_embeddings = self.input_proj(decoder_output) # (batch_size, seq_len, hidden_dim) -> (batch_size, seq_len, latent_dim) 
 
-    for decoder_representations, context_representations in zip(aggregated_decoder, aggregated_context):
-      num_segments = decoder_representations.size(0)
-      if num_segments < max_segments: 
-        padded_decoder = torch.zeros(max_segments - num_segments, decoder_representations.size(1), device=decoder_representations.device)
-        padded_context = torch.zeros(max_segments - num_segments, context_representations.size(1), device=context_representations.device)
-        decoder_representations = torch.cat([decoder_representations, padded_decoder], dim=0)
-        context_representations = torch.cat([context_representations, padded_context], dim=0)
-      padded_decoder.append(decoder_representations)
-      padded_context.append(context_representations)
-    # Final shapes: (batch_size, max_segments, latent_dim)
-    decoder_tensor = torch.stack(padded_decoder, dim=0) # (batch_size, max_segments, latent_dim)
-    context_tensor = torch.stack(padded_context, dim=0) # (batch_size, max_segments, latent_dim)
+    """
+    Step 2: Extract correct SSM/Attention states for each context
 
-    # Fuse representations by concatenating along feature dimension
-    fused = torch.cat([decoder_tensor, context_tensor], dim=-1) # (batch_size, max_segments, 2 * latent_dim)
+    We are definitely taking the easy way out by manually appending the correct context for each subsequence
+    If we have time it would be good to extract the correct states generated from the context so that we do not keep reprocessing the context
+    
+    We perform a vectorized extraction of segmentation indices
+    """
+    # Obtaining the segmentation indices from the segmentation mask
+    
+    # Convert segmentation indices (B, L, 1) to a boolean mask of shape (B, L,)
+    seg_mask = segmentation_indices.squeeze(-1).bool() # (B, L, 1) -> (B, L,)
+    # Create a tensor of positions [0, 1, 2, ..., L - 1] and expand to (B, L)
+    positions = torch.arange(L, device=seg_mask.device).unsqueeze(0).expand(B, L)
+    # Where seg_mask is false, set the positions to L (an out-of-bound placeholder)
+    # NOTE: We use L as L - 1 could be the start of the segment where only the last element is in the segment
+    positions_masked = positions_masked(~seg_mask, L)
 
-    # Predict Gaussian parameters from the fused representation
-    mu_q = self.mu_head(fused) # (batch_size, max_segments, latent_dim)
-    logvar_q = self.logvar_head(fused) # (batch_size, max_segments, latent_dim)
+    # Sort positions along sequence_ dimension; valid positions (lower numbers) will come first
+    positions_sorted, _ = torch.sort(positions_masked, dim=1) # (B, L)
+
+    segmented_decoder_embedings = torch.Tensor([])
+    segmented_correct_embeddings = torch.Tensor([])
+
+    for b in range(B):
+      batch_segment_decoder_embeddings = torch.Tensor([])
+      batch_segment_correct_embeddings = torch.Tensor([])
+
+      batch_segmentation_indices = positions_sorted[b]
+
+      for i in range(len(batch_segmentation_indices)): 
+        start_index = batch_segmentation_indices[i] 
+        end_index = batch_segmentation_indices[i + 1] if i < len(batch_segmentation_indices) - 1 else L
+        
+        batch_segment_decoder_embeddings = torch.cat((batch_segment_decoder_embeddings, decoder_embeddings[b, start_index:end_index, :]), dim=0) # (num_subseq, subseq_len, latent_dim)
+        batch_segment_correct_embeddings = torch.cat((batch_segment_correct_embeddings, correct_embeddings[b, start_index:end_index, :]), dim=0) # (num_subseq, subseq_len, latent_dim)
+      
+      segmented_decoder_embedings = torch.cat((segmented_decoder_embedings, batch_segment_decoder_embeddings.unsqueeze(0)), dim=0) # (batch_size, num_subseq, subseq_len, latent_dim)
+      segmented_correct_embeddings = torch.cat((segmented_correct_embeddings, batch_segment_correct_embeddings.unsqueeze(0)), dim=0) # (batch_size, num_subseq, subseq_len, latent_dim)
+    
+
+    """
+    Step 3: Construct the correct context for each subsequence
+    """
+
+
+    """
+    Step 4: Concatenate context and subsequence and pass through encoder module to obtain hidden state
+    """
+
+    
+    """
+    Step 5: Predict auxiliary Gaussian parameters mu_q and logvar_q given hidden state 
+    """
+    mu_q = self.mu_head() # (batch_size, max_segments, latent_dim)
+    logvar_q = self.logvar_head() # (batch_size, max_segments, latent_dim)
 
     return mu_q, logvar_q
