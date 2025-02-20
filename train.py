437f2d7c77a3bbcb27436442bafeefce61a67f92
PuR3Luck <aidan.ong822@gmail.com>
2025-02-20 16:37:32 +0800

    Get tokeniser to work

--- train.py
+++ train.py
@@ -9,8 +9,7 @@ import torch.nn.functional as F
 import torch.optim as optim 
 
 from torch.utils.data import DataLoader
-from tokenizers import Tokenizer, decoders, normalizers, pre_tokenizers, processors, trainers
-from tokenizers.models import BPE, WordPiece
+from transformers import AutoTokenizer
 from datasets import load_dataset_builder, load_dataset
 
 from serpentvae.modules.SerpentVAE import SerpentVAE
@@ -23,20 +22,10 @@ train_dataset = load_dataset("salesforce/wikitext", "wikitext-2-raw-v1", split="
 test_dataset = load_dataset("salesforce/wikitext", "wikitext-2-raw-v1", split="test")
 val_dataset = load_dataset("salesforce/wikitext", "wikitext-2-raw-v1", split="validation")
 
-# Configure tokenizer
-tokenizer = Tokenizer(WordPiece(unk_token="[UNK]"))
-tokenizer.normalizer = normalizers.BertNormalizer(lowercase=False) # Make the tokeniser cased
-special_tokens = ["[PAD]", "[UNK]", "[CLS]", "[SEP]", "[MASK]","[ENDPHRASE]"]
+# Create tokenizer
+tokenizer = AutoTokenizer.from_pretrained("configs/tokenizer_config")
 
-trainer = trainers.WordPieceTrainer(special_tokens=special_tokens, vocab_size=50000)
-#tokenizer.train_from_iterator(iterator=train_dataset["text"], trainer=trainer)
-
-cls_token_id = tokenizer.token_to_id("[CLS]")
-sep_token_id = tokenizer.token_to_id("[SEP]")
-pad_token_id = tokenizer.token_to_id("[PAD]")
-unk_token_id = tokenizer.token_to_id("[UNK]")
-mask_token_id = tokenizer.token_to_id("[MASK]")
-endphrase_token_id = tokenizer.token_to_id("[ENDPHRASE]")
+print(tokenizer.encode("This is a test", return_tensors = "pt"))
 
 # Create sequences for training and validation
 train_texts = train_dataset["text"] 
