c14d9c4eb4cc144fa0b9c97423521d2949eaf6d8
PuR3Luck <aidan.ong822@gmail.com>
2025-03-01 12:37:52 +0800

    add note

--- lightning_train.py
+++ lightning_train.py
@@ -79,7 +79,7 @@ if __name__ == "__main__":
 
   trainer = pl.Trainer(devices=1,
                        accelerator="gpu",
-                       strategy="auto",
+                       strategy="auto", # "auto" configuration has been tested and works
                        max_epochs = config["train_epochs"],
                        check_val_every_n_epoch = config["eval_freq"],
                        default_root_dir= config["training_path"],
