05bfa01a1b9abd0e7330366c9c119977fdaf85e9
PuR3Luck <aidan.ong822@gmail.com>
2025-03-03 04:29:39 +0800

    turn on torch compile by default

--- stable_lightning_train.py
+++ stable_lightning_train.py
@@ -76,7 +76,7 @@ if __name__ == "__main__":
   train_dataloader, test_dataloader, val_dataloader = prep_dataset(config = config, tokenizer = tokenizer)
 
   # Create model
-  lightning_model = LightningSerpentVAE(config = config, compile_model = True)
+  lightning_model = LightningSerpentVAE(config = config, compile_model = config["compile_model"])
 
   trainer = pl.Trainer(devices=1,
                        accelerator="gpu",
