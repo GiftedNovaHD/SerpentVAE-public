0347df836ce8f20efe5cba8329622297c6a169ca
PuR3Luck <aidan.ong822@gmail.com>
2025-03-15 01:02:19 +0800

    make sure that all functions work as expected

--- test_config.py
+++ test_config.py
@@ -1,5 +1,15 @@
 from train_utils.config_utils import load_config
+from serpentvae.modules.module_utils.layer_parser import layer_parser, get_aliases
+
 
 config = load_config("debug_config")
 
-print(config)
+print(f"Encoder config:{config["encoder"]}")
+
+aliases = get_aliases(config["encoder"])
+
+print(f"Aliases: {aliases}")
+
+layer_config = layer_parser(config["encoder"]["layer_config"], aliases)
+
+print(f"Layer config: {layer_config}")
