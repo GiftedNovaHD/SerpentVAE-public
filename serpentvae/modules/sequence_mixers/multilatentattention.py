45e627bbe91297b2abbde157452b32ea61257046
GiftedNova <cai_junxiang@outlook.com>
2025-03-16 22:41:04 +0800

    feature (MLA block): Mock implementation of MLA block.
    NOT TESTED YET. DO NOT USE.

--- serpentvae/modules/sequence_mixers/multilatentattention.py
+++ serpentvae/modules/sequence_mixers/multilatentattention.py
@@ -30,7 +30,52 @@ class MultiLatentAttention(nn.Module):
     self.qk_head_dim = qk_head_dim
     self.v_head_dim = v_head_dim
 
+    # Low-rank query projection
+    self.q_compress = nn.Linear(hidden_dim, q_lora_rank, bias=False) # Compress the input
+    self.q_up_project = nn.Linear(q_lora_rank, num_heads * hidden_dim, bias=False) # Up-project to produce multi-head queries
+
+
+    # Low-rank key and value projection
+    self.kv_compress = nn.Linear(hidden_dim, kv_lora_rank, bias=False)
+
+    self.kv_up_project = nn.Linear(kv_lora_rank, num_heads * (qk_head_dim + v_head_dim), bias=False) 
+
+    self.output_project = nn.Linear(num_heads * v_head_dim, hidden_dim, bias=False)
+
   def forward(self, x: Tensor, inference_params=None, **kwargs):
     """
     Forward pass of Multi-Latent Attention
     """
+    batch_size, seq_len, hidden_dim = x.size() 
+
+    # Compress and up-project to obtain queries
+    q_compressed = self.q_compress(x) # (batch_size, seq_len, hidden_dim) -> (batch_size, seq_len, num_heads * qk_head_dim)
+    q = self.q_up_project(q_compressed) # (batch_size, seq_len, num_heads * qk_head_dim) -> (batch_size, seq_len, num_heads, qk_head_dim)
+
+    # Key-Value projection
+    # Compress and up-project to get concatenated keys and values
+    kv_compressed = self.kv_compress(x) 
+    kv = self.kv_up_project(kv_compressed) 
+
+    # Split concatenated tensor along last dimension
+    key_dim = self.num_heads * self.qk_head_dim
+    key, value = kv.split([key_dim, self.num_heads * self.v_head_dim], dim=-1)
+
+    # Reshape projections for MHA 
+    q = q.view(batch_size, seq_len, self.num_heads, self.qk_head_dim).transpose(1, 2)
+    key = key.view(batch_size, seq_len, self.num_heads, self.qk_head_dim).transpose(1, 2) 
+    value = value.view(batch_size, seq_len, self.num_heads, self.v_head_dim).transpose(1, 2) 
+
+    scaling = 1.0 / torch.sqrt(self.qk_head_dim)
+    q = q * scaling 
+
+    attention_scores = torch.matmul(q, key.transpose(-2, -1)) 
+    attention_probs = torch.softmax(attention_scores, dim=-1)
+
+    attention_output = torch.matmul(attention_probs, value)
+
+    attention_output = attention_output.transpose(1, 2).contiguous().view(batch_size, seq_len, self.num_heads * self.v_head_dim)
+
+    final_output = self.output_project(attention_output)
+
+    return final_output
