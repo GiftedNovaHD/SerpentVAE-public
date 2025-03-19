1c703e0872ddf25813706cf2349fae441fa6e96a
PuR3Luck <aidan.ong822@gmail.com>
2025-03-20 00:00:08 +0800

    quick fix

--- serpentvae/modules/SerpentVAE.py
+++ serpentvae/modules/SerpentVAE.py
@@ -818,7 +818,7 @@ class SerpentVAE(nn.Module):
       # Normalize the logits so softmax is not dominated by the largest logit
       decoded_outputs = decoded_outputs / (self.hidden_dim ** 0.5)
 
-      #print(f"Decoded logits max: {decoded_logits.max()}, min: {decoded_logits.min()}")
+      #print(f"Decoded logits max: {decoded_outputs.max()}, min: {decoded_outputs.min()}")
     
     else: # Continuous input
       decoded_outputs = decoded_hidden_tokens # (batch_size, seq_len, hidden_dim)
