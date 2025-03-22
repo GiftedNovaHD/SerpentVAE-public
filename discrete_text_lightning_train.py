c140729948c0eac5a7501e3f254399b79e1126b4
GiftedNova <cai_junxiang@outlook.com>
2025-03-22 18:38:18 +0800

    Revert repo to state of commit e0b6cd37cc696842753ab6fd76188869fe7cfaf5

--- discrete_text_lightning_train.py
+++ discrete_text_lightning_train.py
@@ -71,7 +71,7 @@ if __name__ == "__main__":
   parallelism_strategy = prep_parallelism(config = config)
 
   checkpoint_callback = ModelCheckpoint(dirpath = config["training_path"],
-                                        every_n_epochs = 1, # Save at the end of every epoch
+                                        every_n_train_steps = config["checkpoint_freq"],
                                         verbose = True, 
                                         save_last = True
                                       )
@@ -86,29 +86,30 @@ if __name__ == "__main__":
                        strategy=parallelism_strategy, # FSDP Strategy
                        use_distributed_sampler = True,
                        max_epochs = config["num_epochs"],
+                       val_check_interval = config["eval_freq"],
                        limit_val_batches = 1,
                        default_root_dir= config["training_path"],
                        profiler = "pytorch",
                        callbacks = [ModelSummary(max_depth = 5), checkpoint_callback, memory_monitor]
                       )
 
-  trainer.fit(model = lightning_model, train_dataloaders = train_dataloader, val_dataloaders = val_dataloader)
+  #trainer.fit(model = lightning_model, train_dataloaders = train_dataloader, val_dataloaders = val_dataloader)
   trainer.print(torch.cuda.memory_summary())
 
-  # Resume training. Comment out the above trainer.fit() and uncomment the following code to resume training
-  # checkpoint_path = os.path.join(config["training_path"], "last.ckpt") # Default path
+  # Resume training
+  checkpoint_path = os.path.join(config["training_path"], "last.ckpt") # Default path
 
-  # # # Check if 'last.ckpt' exists, if not find the latest .ckpt file
-  # if not os.path.exists(checkpoint_path):
-  #   # Find all .ckpt files in the training directory
-  #   checkpoint_files = [f for f in os.listdir(config["training_path"]) if f.endswith(".ckpt")]
+  # Check if 'last.ckpt' exists, if not find the latest .ckpt file
+  if not os.path.exists(checkpoint_path):
+    # Find all .ckpt files in the training directory
+    checkpoint_files = [f for f in os.listdir(config["training_path"]) if f.endswith(".ckpt")]
 
-  #   if checkpoint_files:
-  #     # Get the most recently modified checkpoint file
-  #     checkpoint_path = os.path.join(config["training_path"], max(checkpoint_files, key=os.path.getmtime))
-  #     print(f"Resuming from latest checkpoint: {checkpoint_path}")
-  #   else:
-  #     print("No checkpoint found. Starting from scratch.")
-  #     checkpoint_path = None  # Or handle the case where no checkpoint exists
+    if checkpoint_files:
+      # Get the most recently modified checkpoint file
+      checkpoint_path = os.path.join(config["training_path"], max(checkpoint_files, key=lambda f: os.path.getmtime(os.path.join(config["training_path"], f))))
+      print(f"Resuming from latest checkpoint: {checkpoint_path}")
+    else:
+      print("No checkpoint found. Starting from scratch.")
+      checkpoint_path = None  # Or handle the case where no checkpoint exists
 
-  # trainer.fit(model = lightning_model, train_dataloaders = train_dataloader, val_dataloaders = val_dataloader, ckpt_path = checkpoint_path)
+  trainer.fit(model = lightning_model, train_dataloaders = train_dataloader, val_dataloaders = val_dataloader, ckpt_path = checkpoint_path)
--- train_utils/prep_text_dataloaders.py
+++ train_utils/prep_text_dataloaders.py
@@ -1,122 +1,102 @@
-import os
+import os 
 import psutil
+
 from typing import Dict, Tuple
 from datasets import load_dataset_builder, load_dataset
-from torch.utils.data import DataLoader, IterableDataset
+from torch.utils.data import DataLoader
 
-class ResumableIterableDataset(IterableDataset):
-    def __init__(self, dataset, start_index=0):
-        self.dataset = dataset
-        self.start_index = start_index
-        self.current_index = start_index
+def prep_text_dataset(config: Dict,tokenizer) -> Tuple[DataLoader, DataLoader, DataLoader]:
+  """
+  Takes in the configuration and returns dataloaders for the training, testing, and validation datasets.
 
-    def __iter__(self):
-        for i, item in enumerate(self.dataset):
-            if i < self.start_index:
-                continue
-            self.current_index = i
-            yield item["text"]  # Access "text" field here
+  Args:
+    config (dict): The configuration dictionary for the given experiment
+      - "dataset_path" (str): The path to the dataset
+      - "dataset_name" (str): The name of the dataset
+  Returns:
+    train_dataloader (DataLoader): The training dataloader
+    test_dataloader (DataLoader): The testing dataloader
+    val_dataloader (DataLoader): The validation dataloader
+  """
+  # NOTE: Using smallest possible version for testing
+  dataset_builder = load_dataset_builder(path = config["dataset_path"], name = config["dataset_name"])
 
-    def state_dict(self):
-        return {'start_index': self.current_index}
+  # Load datasets
+  train_dataset = load_dataset(path = config["dataset_path"], name = config["dataset_name"], split = "train")
+  test_dataset = load_dataset(path = config["dataset_path"], name = config["dataset_name"], split = "test")
+  val_dataset = load_dataset(path = config["dataset_path"], name = config["dataset_name"], split = "validation")
 
-    def load_state_dict(self, state_dict):
-        self.start_index = state_dict['start_index']
-        self.current_index = self.start_index
+  # Filter datasets to remove blank sequences
+  def filter_empty(sequence):
+    return not ((sequence["text"].strip() == "\n") or (sequence["text"].strip() == ""))
 
-def prep_text_dataset(config: Dict, tokenizer) -> Tuple[DataLoader, DataLoader, DataLoader]:
+  filtered_train_dataset = train_dataset.filter(filter_empty)
+  filtered_test_dataset = test_dataset.filter(filter_empty)
+  filtered_val_dataset = val_dataset.filter(filter_empty)
+
+  # Create sequences for training and validation
+  train_texts = filtered_train_dataset["text"][1:]
+  test_texts = filtered_test_dataset["text"][1:]
+  val_texts = filtered_val_dataset["text"][1:]
+
+  print(f"Number of training sequences: {len(train_texts)}")
+  print(f"Number of testing sequences: {len(test_texts)}")
+  print(f"Number of validation sequences: {len(val_texts)}")
+
+
+  def collate(batch):
     """
-    Takes in the configuration and returns dataloaders for the training, testing, and validation datasets.
-
-    Args:
-        config (dict): The configuration dictionary for the given experiment
-        - "dataset_path" (str): The path to the dataset
-        - "dataset_name" (str): The name of the dataset
-    Returns:
-        train_dataloader (DataLoader): The training dataloader
-        test_dataloader (DataLoader): The testing dataloader
-        val_dataloader (DataLoader): The validation dataloader
+    Tokenizes the batch of sequences.
     """
-    # NOTE: Using smallest possible version for testing
-    dataset_builder = load_dataset_builder(path = config["dataset_path"], name = config["dataset_name"])
+    return tokenizer(batch, padding = True, truncation = True, max_length = config["max_seq_len"], return_tensors = "pt")
+  
+  # Get number of workers for DataLoaders
+  if config["dataloader_num_workers"] is None:
+    dataloader_num_workers = min(16, count_workers())
+  else:
+    dataloader_num_workers = config["dataloader_num_workers"]
 
-    # Load datasets
-    train_dataset = load_dataset(path = config["dataset_path"], name = config["dataset_name"], split = "train", streaming=True)
-    test_dataset = load_dataset(path = config["dataset_path"], name = config["dataset_name"], split = "test", streaming=True)
-    val_dataset = load_dataset(path = config["dataset_path"], name = config["dataset_name"], split = "validation", streaming=True)
+  print(f"Number of workers for DataLoaders: {dataloader_num_workers}")
+  
+  train_dataloader = DataLoader(dataset = train_texts,
+                                batch_size = config["batch_size"],
+                                shuffle = True,
+                                collate_fn = collate,
+                                num_workers = dataloader_num_workers,
+                                persistent_workers = True,
+                                pin_memory = True,
+                                pin_memory_device = config["device"]
+                               )
+  
+  test_dataloader = DataLoader(dataset = test_texts,
+                               batch_size = config["batch_size"],
+                               shuffle = False,
+                               collate_fn = collate,
+                               num_workers = dataloader_num_workers,
+                               persistent_workers = True,
+                               pin_memory = True,
+                               pin_memory_device = config["device"]
+                              )
+  
+  val_dataloader = DataLoader(dataset = val_texts,
+                              batch_size = config["batch_size"],
+                              shuffle = False,
+                              collate_fn = collate,
+                              num_workers = dataloader_num_workers,
+                              persistent_workers = True,
+                              pin_memory = True,
+                              pin_memory_device = config["device"]
+                             )
 
-    # Filter datasets to remove blank sequences
-    def filter_empty(sequence):
-        return not ((sequence["text"].strip() == "\n") or (sequence["text"].strip() == ""))
+  return train_dataloader, test_dataloader, val_dataloader
 
-    filtered_train_dataset = train_dataset.filter(filter_empty)
-    filtered_test_dataset = test_dataset.filter(filter_empty)
-    filtered_val_dataset = val_dataset.filter(filter_empty)
+def count_workers() -> int: 
+  try: 
+    vCPUs = os.cpu_count() 
 
-    # Calculate approximate lengths by iterating
-    num_train_sequences = sum(1 for _ in filtered_train_dataset)
-    num_test_sequences = sum(1 for _ in filtered_test_dataset)
-    num_val_sequences = sum(1 for _ in filtered_val_dataset)
-
-    print(f"Number of training sequences: {num_train_sequences}")
-    print(f"Number of testing sequences: {num_test_sequences}")
-    print(f"Number of validation sequences: {num_val_sequences}")
-
-    def collate(batch):
-        """
-        Tokenizes the batch of sequences.
-        """
-        return tokenizer(batch, padding = True, truncation = True, max_length = config["max_seq_len"], return_tensors = "pt")
-
-    # Get number of workers for DataLoaders
-    if config["dataloader_num_workers"] is None:
-        dataloader_num_workers = min(16, count_workers())
-    else:
-        dataloader_num_workers = config["dataloader_num_workers"]
-
-    print(f"Number of workers for DataLoaders: {dataloader_num_workers}")
-
-    # Wrap the datasets with ResumableIterableDataset
-    train_dataset = ResumableIterableDataset(filtered_train_dataset)
-    test_dataset = ResumableIterableDataset(filtered_test_dataset)
-    val_dataset = ResumableIterableDataset(filtered_val_dataset)
-
-    train_dataloader = DataLoader(dataset=train_dataset,
-                                  batch_size=config["batch_size"],
-                                  shuffle=False,  # Remove shuffle
-                                  collate_fn=collate,
-                                  num_workers=dataloader_num_workers,
-                                  persistent_workers=True,
-                                  pin_memory=True,
-                                  pin_memory_device=config["device"])
-
-    test_dataloader = DataLoader(dataset=test_dataset,
-                                 batch_size=config["batch_size"],
-                                 shuffle=False,
-                                 collate_fn=collate,
-                                 num_workers=dataloader_num_workers,
-                                 persistent_workers=True,
-                                 pin_memory=True,
-                                 pin_memory_device=config["device"])
-
-    val_dataloader = DataLoader(dataset=val_dataset,
-                                batch_size=config["batch_size"],
-                                shuffle=False,
-                                collate_fn=collate,
-                                num_workers=dataloader_num_workers,
-                                persistent_workers=True,
-                                pin_memory=True,
-                                pin_memory_device=config["device"])
-
-    return train_dataloader, test_dataloader, val_dataloader
-
-def count_workers() -> int:
-    try:
-        vCPUs = os.cpu_count()
-
-        if vCPUs is None:
-            vCPUs = psutil.cpu_count(logical=True)
-
-        return vCPUs
-    except Exception as e:
-        return 1
+    if vCPUs is None: 
+      vCPUs = psutil.cpu_count(logical=True) 
+    
+    return vCPUs
+  except Exception as e: 
+    return 1 
