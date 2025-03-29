3783cf1f1a9f3be4017fa7cac60c81e8d6e2a1cb
PuR3Luck <aidan.ong822@gmail.com>
2025-03-30 00:27:01 +0800

    more experiments

--- experiments/load_audio_dataset_experiments.py
+++ experiments/load_audio_dataset_experiments.py
@@ -6,5 +6,6 @@ from io import BytesIO
 dataset = load_dataset(path="GiftedNova/tokenized-voxpopuli", split="train")
 
 for sample in dataset:
+  print(type(sample["pt"]))
   print(torch.load(BytesIO(sample["pt"])))
   break
