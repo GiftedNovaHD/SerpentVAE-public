a698b28412e34311c50fcae818065ba133b09766
GiftedNova <cai_junxiang@outlook.com>
2025-04-02 12:18:58 +0800

    style (scaled normal): whitespace

--- serpentvae/modules/distributions/scaled_normal.py
+++ serpentvae/modules/distributions/scaled_normal.py
@@ -172,7 +172,7 @@ class ScaledNormal(nn.Module):
     kl_per_dim = sigma_squared + mu.pow(2) - 1.0 - logvar # (batch, seq_len, hidden_dim)
     
     # Mean over latent dimension
-    kl = 0.5 * torch.mean(kl_per_dim, dim=-1) # (batch, seq_len)
+    kl = 0.5 * torch.mean(kl_per_dim, dim = -1) # (batch, seq_len)
     
     # Average over both the batch and sequence length dimensions to obtain a scalar
     kl = kl.mean()  # scalar value
